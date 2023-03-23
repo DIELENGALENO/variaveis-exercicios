b3352c9d7778be6362f4f34a47faca3c9ab95414
DIELEN <dielen_lemos@hotmail.com>
2023-03-23 14:13:38 +0100

    Exercício2

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+let a = 10
+let b = 25
+let c 
+
+
+
+
+//a = b   // a = 25
+//b = a   // b = 25
+
+
+c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+
